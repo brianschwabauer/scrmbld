85cd1d91a53dd106852852f3e7ffe0d79a528375
Brian Schwabauer <brian@brianschwabauer.com>
2024-11-15 13:54:12 -0600

    Change layout & move timer between action buttons

--- src/routes/playtest/+page.svelte
+++ src/routes/playtest/+page.svelte
@@ -3,10 +3,14 @@
 	import { assets } from '$app/paths';
 	import { randomNumberGenerator } from '$lib';
 	import FlipText from '$lib/FlipText.svelte';
+	import Keyboard from '$lib/Keyboard.svelte';
 	import Popover from '$lib/Popover.svelte';
+	import { ripple } from '$lib/ripple';
 	import { tooltip } from '$lib/tootltip.js';
 	import { untrack } from 'svelte';
 	import { Confetti } from 'svelte-confetti';
+	import { quartInOut } from 'svelte/easing';
+	import { type TransitionConfig } from 'svelte/transition';
 
 	const { data } = $props();
 	const words = $derived(data.words);
@@ -18,12 +22,13 @@
 	let inputEl = $state<HTMLInputElement | undefined>(undefined);
 	let answerEl = $state<HTMLDivElement | undefined>(undefined);
 	let shuffling = $state(false);
-	let scrambled = $state(shuffle());
+	let scrambled = $state(shuffle(false));
 	let attempt = $state('');
 	let selectionStart = $state(0);
 	let selectionEnd = $state(0);
-	let success = $state(false);
 	let times = $state<number[][]>([]);
+	let hintLetters = $state(0);
+	const success = $derived(!!answer && attempt === answer);
 	const time = $derived(
 		times.reduce((total, [start, end]) => {
 			return total + Math.max(0, Math.round((end - start) / 1000));
@@ -49,13 +54,18 @@
 		return `${minutes}:${seconds.toString().padStart(2, '0')}`;
 	});
 	const usedLetters = $derived.by(() => {
-		const letters = new Set<number>();
-		attempt.split('').forEach((letter) => {
-			let index = scrambled.indexOf(letter);
-			if (letters.has(index)) index = scrambled.indexOf(letter, index + 1);
-			if (index > -1) letters.add(index);
+		const letterIndexes = new Set<number>();
+		attempt.split('').forEach((letter, i) => {
+			let index: number | undefined;
+			while (index === undefined || index > -1 || index >= scrambled.length - 1) {
+				index = scrambled.indexOf(letter, (index ?? -1) + 1);
+				if (index > -1 && !letterIndexes.has(index)) {
+					letterIndexes.add(index);
+					break;
+				}
+			}
 		});
-		return letters;
+		return letterIndexes;
 	});
 	const shareURL = `https://scrmbld.app`;
 	const shareText = $derived(`🅂🄲🅁🄼🄱🄻🄳 ⏲${timeDisplay}`);
@@ -92,8 +102,31 @@
 		}, 5000);
 	}
 
-	function shuffle() {
+	const audioEffects = browser
+		? [{ start: 0, end: 2.5, audio: new Audio(`${assets}/splitflap.mp3`) }]
+		: [];
+	audioEffects.forEach((effect) => effect.audio.load());
+	function playSoundEffect(effect: number) {
+		if (!audioEffects[effect]) return;
+		const audio = audioEffects[effect].audio;
+		const isPlaying =
+			audio.currentTime > 0 && !audio.paused && !audio.ended && audio.readyState > 2;
+		if (isPlaying) return;
+		audio.currentTime = audioEffects[effect].start;
+		audio.play();
+		const cb = () => {
+			if (audio.currentTime >= audioEffects[effect].end) {
+				audio.pause();
+				audio.removeEventListener('timeupdate', cb);
+			}
+		};
+		audio.addEventListener('timeupdate', cb);
+	}
+	$effect(() => playSoundEffect(0));
+
+	function shuffle(playSound = true) {
 		shuffling = true;
+		if (playSound) playSoundEffect(0);
 		setTimeout(() => {
 			shuffling = false;
 		}, 1500);
@@ -105,66 +138,73 @@
 		return letters.join('').toUpperCase();
 	}
 
-	function removeExtraLetter() {
-		if (mixletters.length === 0) return;
-		mixletters.pop();
-		scrambled = shuffle();
+	function applyHint() {
+		if (mixletters.length) {
+			mixletters.pop();
+			scrambled = shuffle();
+			return;
+		}
+		if (hintLetters >= answer.length) return;
+		hintLetters++;
+		attempt = answer.slice(0, hintLetters) + attempt.slice(hintLetters);
 	}
 
-	let timer: ReturnType<typeof setTimeout> | undefined;
 	function onWindowKeyUp(e: KeyboardEvent) {
-		function debounceFocus() {
-			clearTimeout(timer);
-			timer = setTimeout(() => {
-				inputEl?.focus();
-			}, 100);
-		}
 		if (!inputEl) return;
 		if (e.target === inputEl || inputEl === document.activeElement) return;
 		if (e.ctrlKey || e.metaKey || e.altKey) return;
 		if (e.key === 'ArrowLeft') {
 			selectionStart = Math.max(selectionStart - 1, 0);
 			selectionEnd = Math.max(selectionStart, 1);
-			debounceFocus();
 			inputEl.setSelectionRange(selectionStart, selectionEnd);
 			return;
 		}
 		if (e.key === 'ArrowRight') {
-			selectionEnd = Math.min(selectionEnd + 1, 7);
+			selectionEnd = Math.min(selectionEnd + 1, 7, attempt.length - hintLetters);
 			selectionStart = Math.min(selectionEnd, 6);
-			debounceFocus();
 			inputEl.setSelectionRange(selectionStart, selectionEnd);
 			return;
 		}
 		if (e.key === 'Backspace') {
 			selectionStart = Math.max(Math.min(selectionStart, selectionEnd - 1), 0);
-			attempt = attempt.slice(0, selectionStart) + attempt.slice(selectionEnd);
+			attempt =
+				answer.slice(0, hintLetters) +
+				attempt.slice(hintLetters, hintLetters + selectionStart) +
+				attempt.slice(hintLetters + selectionEnd);
 			selectionStart++;
 			selectionEnd = selectionStart;
-			debounceFocus();
 			inputEl.setSelectionRange(selectionStart, selectionEnd);
 			return;
 		}
 		if (!e.key.match(/^[A-Za-z]$/)) return;
 		const key = e.key.toUpperCase();
-		attempt = attempt.slice(0, selectionStart) + key + attempt.slice(selectionEnd);
+		attempt =
+			answer.slice(0, hintLetters) +
+			attempt.slice(hintLetters, hintLetters + selectionStart) +
+			key +
+			attempt.slice(hintLetters + selectionEnd);
 		selectionStart++;
 		selectionEnd = selectionStart;
-		debounceFocus();
 		inputEl.setSelectionRange(selectionStart, selectionEnd);
 	}
 
-	$effect(() => {
-		if (inputEl) {
-			inputEl.focus();
-		}
-	});
+	function hintProgressTransition(node: HTMLElement): TransitionConfig {
+		return {
+			easing: quartInOut,
+			duration: 500,
+			css: (t, u) => {
+				return `
+					width: calc(var(--size) * ${t});
+					opacity: ${t};
+				`;
+			}
+		};
+	}
 
 	$effect(() => {
 		if (success) return;
 		if (answer && attempt === answer) {
 			if (times[times.length - 1]) times[times.length - 1][1] = Date.now();
-			success = true;
 			clearInterval(interval);
 			localStorage.setItem(
 				`scrmbld_${todaysWord.day}`,
@@ -201,10 +241,7 @@
 				const savedInfo = JSON.parse(localStorage.getItem(`scrmbld_${todaysWord.day}`) || '');
 				if (savedInfo) {
 					times = savedInfo.times;
-					success = savedInfo.success ?? false;
-					if (success) {
-						attempt = answer;
-					}
+					if (savedInfo.success) attempt = answer;
 				}
 			} catch (error) {
 				// ignore
@@ -233,6 +270,10 @@
 	});
 </script>
 
+<svelte:head>
+	<title>Play | SCRMBLD</title>
+</svelte:head>
+
 <svelte:window onkeyup={onWindowKeyUp} />
 
 {#if attempt === answer}
@@ -249,33 +290,32 @@
 {/if}
 
 <article>
-	<div class="timer">
-		<!-- <FlipText
-			word={timeDisplay}
-			minLength={4}
-			duration={200}
-			alphabet={[':', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']}
-		/> -->
-	</div>
-	<div class="question">
-		<FlipText word="          " />
-		<FlipText word={' ' + scrambled.padEnd(9, ' ')} {usedLetters} />
-		<FlipText word="          " />
-	</div>
 	<div class="answer" bind:this={answerEl}>
+		{#if hintLetters}
+			<FlipText
+				class="hint"
+				word={answer.slice(0, hintLetters)}
+				duration={100}
+				onlyAnimateOneLetter
+				success
+				minLength={hintLetters}
+			/>
+		{/if}
 		<FlipText
-			word={attempt}
+			word={attempt.slice(hintLetters)}
 			duration={100}
 			{selectionEnd}
 			{selectionStart}
 			onlyAnimateOneLetter
-			success={attempt === answer}
-			error={attempt.length === answer.length && attempt !== answer}
+			{success}
+			error={attempt.length === answer.length && !success}
+			minLength={answer.length - hintLetters}
 		/>
 		<input
 			bind:this={inputEl}
 			type="text"
-			value={attempt}
+			style:--num-letters={answer.length - hintLetters}
+			value={attempt.slice(hintLetters)}
 			onkeyup={(e) => {
 				const target =e.target as HTMLInputElement;
 				selectionEnd = Math.min(target.selectionEnd as number, 7);
@@ -295,7 +335,7 @@
 				newValue = newValue.slice(0, 7);
 				selectionEnd = Math.min(selectionEnd - numberOfInvaidChars, 7);
 				selectionStart = Math.min(selectionStart - numberOfInvaidChars, selectionEnd, 6);
-				attempt = newValue;
+				attempt = answer.slice(0, hintLetters) + newValue;
 				if (newValue !== value) {
 					target.value = attempt;
 					target.setSelectionRange(selectionStart, selectionEnd);
@@ -312,7 +352,51 @@
 			}}
 		/>
 	</div>
+	<div class="question">
+		<FlipText word={scrambled} {usedLetters} duration={350} />
+	</div>
 	<div class="actions">
+		{#if success}
+			<button
+				use:ripple
+				onclick={() => {
+					attempt = '';
+					times = [];
+					hintLetters = 0;
+					localStorage.removeItem(`scrmbld_${todaysWord.day}`);
+				}}>Reset</button
+			>
+		{:else}
+			<button disabled={shuffling} onclick={() => (scrambled = shuffle())} use:ripple
+				>Shuffle</button
+			>
+		{/if}
+		<div class="timer">
+			<FlipText
+				word={timeDisplay}
+				minLength={4}
+				duration={200}
+				alphabet={[
+					'',
+					'@',
+					'#',
+					'+',
+					'=',
+					'?',
+					':',
+					'0',
+					'1',
+					'2',
+					'3',
+					'4',
+					'5',
+					'6',
+					'7',
+					'8',
+					'9'
+				]}
+			/>
+		</div>
 		{#if success}
 			<button class="primary" onclick={openNativeShare} bind:this={shareButtonEl}>Share</button>
 			{#if !useNativeShare}
@@ -324,6 +408,7 @@
 								onclick={shareToClipboard}
 								aria-label="Copy share text"
 								use:tooltip={'Copy score to clipboard'}
+								use:ripple
 							>
 								<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"
 									><path
@@ -336,6 +421,7 @@
 								onclick={shareOnTwitter}
 								class="twitter"
 								use:tooltip={'Share score on Twitter/X'}
+								use:ripple
 							>
 								<img src="{assets}/x.svg" alt="Twitter Logo" />
 							</button>
@@ -343,6 +429,7 @@
 								onclick={shareOnFacebook}
 								class="facebook"
 								use:tooltip={'Share score on Facebook'}
+								use:ripple
 							>
 								<img src="{assets}/facebook.png" alt="Facebook Logo" />
 							</button>
@@ -354,18 +441,34 @@
 				</Popover>
 			{/if}
 		{:else}
-			<button disabled={shuffling} onclick={() => (scrambled = shuffle())}>Shuffle</button>
-			{#if mixletters.length > 0}
-				<button onclick={() => removeExtraLetter()}>Hint</button>
-			{/if}
+			{@const hintEnabled = time > (mixletters.length ? 60 : 120 + hintLetters * 60)}
+			<button onclick={() => applyHint()} use:ripple class="hint" disabled={!hintEnabled}>
+				{#if !hintEnabled && browser && time}
+					<div class="hint-progress" transition:hintProgressTransition>
+						<svg viewBox="0 0 100 100" style:--progress={((time % 60) / 60) * 100}>
+							<circle class="bg"></circle>
+							<circle class="fg"></circle>
+						</svg>
+					</div>
+				{/if}
+				Hint
+			</button>
 		{/if}
 	</div>
+	<Keyboard
+		onclick={(key) => {
+			if (key === 'Clear') {
+				attempt = '';
+				selectionStart = 0;
+				selectionEnd = 0;
+				return;
+			}
+			window.dispatchEvent(new KeyboardEvent('keyup', { key }));
+		}}
+	></Keyboard>
 </article>
 
 <style lang="scss">
-	.timer {
-		margin-bottom: 1rem;
-	}
 	.confetti {
 		position: fixed;
 		bottom: 0;
@@ -378,10 +481,16 @@
 		max-width: 100vw;
 		overflow: hidden;
 		min-height: 100vh;
-		padding: 2rem 0;
+		padding: 4rem 0;
 		@media (min-width: 768px) {
 			justify-content: center;
 			padding: 0;
+		}
+		@media (max-width: 769px) {
+			:global(.keyboard) {
+				position: fixed;
+				bottom: 4rem;
+			}
 		}
 	}
 	.actions {
@@ -393,20 +502,84 @@
 		margin-top: 2rem;
 		margin-bottom: 4rem;
 	}
+	.hint {
+		display: flex;
+		align-items: center;
+	}
+	.hint-progress {
+		--width: 1.5rem;
+		--stroke-width: 15px;
+		width: var(--width);
+		height: var(--width);
+		position: relative;
+		margin: 0 0.5rem 0 -0.5rem;
+		svg {
+			position: absolute;
+			top: 0;
+			left: 0;
+			width: var(--width);
+			height: var(--width);
+			--size: 100px;
+			--half-size: calc(var(--size) / 2);
+			--radius: calc((var(--size) - var(--stroke-width)) / 2);
+			--circumference: calc(var(--radius) * pi * 2);
+			--dash: calc((var(--progress) * (var(--circumference) - var(--stroke-width))) / 100);
+		}
+
+		svg circle {
+			cx: var(--half-size);
+			cy: var(--half-size);
+			r: var(--radius);
+			stroke-width: var(--stroke-width);
+			fill: none;
+			stroke-linecap: round;
+		}
+
+		svg circle.bg {
+			stroke: rgba(255, 255, 255, 0.2);
+		}
+
+		svg circle.fg {
+			transform: rotate(-90deg);
+			transform-origin: var(--half-size) var(--half-size);
+			stroke-dasharray: var(--dash) calc(var(--circumference) - var(--dash));
+			transition: stroke-dasharray 0.3s linear 0s;
+			stroke: rgba(255, 255, 255, 0.9);
+		}
+
+		@property --progress {
+			syntax: '<number>';
+			inherits: false;
+			initial-value: 0;
+		}
+	}
 	button {
+		-webkit-tap-highlight-color: transparent;
+		position: relative;
 		cursor: pointer;
-		font-size: 1.5rem;
+		font-size: 1rem;
 		text-decoration: none;
 		border-radius: 999px;
-		padding: 0.5rem 1.5rem;
+		padding: 0.5em 1em;
 		margin: 0;
 		font-weight: 500;
 		outline: none;
 		box-shadow: none;
 		border: none;
 		background-color: rgba(255, 255, 255, 0.05);
-		color: #eeeeee;
-		&:hover {
+		color: #dddddd;
+		font-family: 'Roboto Mono', monospace;
+		font-optical-sizing: auto;
+		font-weight: 400;
+		font-style: normal;
+		@media (min-width: 600px) {
+			font-size: 1.5rem;
+		}
+		&:disabled {
+			opacity: 0.65;
+			cursor: not-allowed;
+		}
+		&:hover:not(:disabled) {
 			background-color: rgba(255, 255, 255, 0.1);
 		}
 		&.primary {
@@ -421,9 +594,8 @@
 	.question {
 		display: flex;
 		flex-direction: column;
-		gap: 0.5rem;
+		gap: 1rem;
 		font-size: 1rem;
-		margin-bottom: 1rem;
 		@media (min-width: 350px) {
 			font-size: 1.35rem;
 		}
@@ -446,25 +618,32 @@
 		grid-template-columns: 1fr;
 		grid-template-rows: 1fr;
 		font-size: 1rem;
+		margin-bottom: 1rem;
 		@media (min-width: 350px) {
 			font-size: 2rem;
 		}
 		@media (min-width: 400px) {
 			font-size: 2rem;
 		}
-		@media (min-width: 1200px) {
+		@media (min-width: 600px) {
 			font-size: 3rem;
 		}
+		@media (min-width: 1200px) {
+			font-size: 4rem;
+		}
 		@media (min-width: 1600px) {
-			font-size: 3.5rem;
+			font-size: 4.5rem;
 		}
 		:global(.flip-text) {
 			grid-row: 1 / 1;
+			grid-column: 2 / 2;
+		}
+		:global(.flip-text.hint) {
 			grid-column: 1 / 1;
 		}
 		input {
 			grid-row: 1 / 1;
-			grid-column: 1 / 1;
+			grid-column: 2 / 2;
 			z-index: 2;
 			background-color: transparent;
 			border: none;
@@ -475,7 +654,7 @@
 			font-optical-sizing: auto;
 			font-weight: 500;
 			font-style: normal;
-			width: calc(0.8em * 7);
+			width: calc(0.8em * var(--num-letters));
 			padding: 0 0 0 0.1em;
 			margin: 0;
 			line-height: 1em;
