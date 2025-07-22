8859ba67e6244eda2c5dc17756975668f072f131
Brian Schwabauer <brian@brianschwabauer.com>
2025-07-22 10:51:10 -0500

    Add wrangler D1 locally for testing

---
+++ migrations/0001_add-gameplay-table.sql
@@ -0,0 +1,25 @@
+-- Migration number: 0001 	 2025-07-21T22:25:05.310Z
+CREATE TABLE IF NOT EXISTS gameplay(
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	uuid TEXT NOT NULL,
+	word TEXT NOT NULL,
+	day INTEGER NOT NULL,
+	user_uuid TEXT NOT NULL,
+	started_at INTEGER NOT NULL,
+	ended_at INTEGER,
+	time NUMERIC,
+	num_hints INTEGER,
+	json TEXT,
+	ip TEXT,
+	ip_city TEXT,
+	ip_country TEXT,
+	ip_latitude TEXT,
+	ip_longitude TEXT,
+	ip_region TEXT,
+	ip_timezone TEXT,
+	ua TEXT,
+	ua_browser TEXT,
+	ua_os TEXT,
+	ua_device TEXT,
+	ua_version
+);
